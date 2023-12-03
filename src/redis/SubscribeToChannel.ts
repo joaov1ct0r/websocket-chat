5e7f26651283537976824a6ec51fdd7ed21bec6b
joaov1ct0r <jov9051@outlook.com>
2023-12-03 13:38:35 -0300

    feat: redis subscribe to channel

--- src/redis/SubscribeToChannel.ts
+++ src/redis/SubscribeToChannel.ts
@@ -1,20 +1,18 @@
-import Redis from 'ioredis'
+import { BaseRedis } from '@Redis/BaseRedis'
 
 export interface RedisSubscribeToChannelImp {
-  execute(
-    redis: Redis,
-    channel: string,
-    callback: (message: string) => void,
-  ): Promise<void>
+  execute(channel: string, callback: (message: string) => void): Promise<void>
 }
 
-export class RedisSubscribeToChannel implements RedisSubscribeToChannelImp {
+export class RedisSubscribeToChannel
+  extends BaseRedis
+  implements RedisSubscribeToChannelImp
+{
   public async execute(
-    redis: Redis,
     channel: string,
     callback: (message: string) => void,
   ): Promise<void> {
-    await redis.subscribe(channel, (error, result) => {
+    await this._redis.subscribe(channel, (error, result) => {
       if (error) {
         throw new Error(`Erro no redis - ${error}`)
       }
