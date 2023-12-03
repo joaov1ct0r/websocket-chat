f25173b1fa148ee425659fe8785b98bf846fefdf
joaov1ct0r <jov9051@outlook.com>
2023-12-03 13:40:37 -0300

    feat: index

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
 import http from 'http'
 import express from 'express'
 import SocketIO from '@Socket'
-import IORedis from '@Redis'
+import { RedisClient } from '@Redis/client'
 
 const app = express()
 const server = http.createServer(app)
-const redis = new IORedis(
-  String(process.env.REDIS_HOST),
-  Number(process.env.REDIS_PORT),
-)
+const redis = new RedisClient()
 const socket = new SocketIO(Number(process.env.SOCKET_PORT), server, redis)
 socket.init()
