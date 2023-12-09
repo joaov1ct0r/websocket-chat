c70a71bdf228c4784a0434ac90ae572563a31c95
joaov1ct0r <jov9051@outlook.com>
2023-12-09 15:26:30 -0300

    refactor: socket

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,13 +1,11 @@
 import { Server, Socket } from 'socket.io'
 import { Server as httpServer } from 'http'
 import { SocketGetMessageImp } from '@Socket/GetMessage'
-import { SocketAddMessageImp } from '@Socket/AddMessage'
-import { SocketPublishMessageImp } from '@Socket/PublishMessage'
 import { SocketSubscribeToChannelImp } from '@Socket/SubscribeToChannel'
 import { SocketWelcomeImp } from '@Socket/Welcome'
 import { SocketBroadcastNewConnectionImp } from '@Socket/BroadcastNewConnection'
 import { SocketEmitMessagesImp } from '@Socket/EmitMessages'
-import { SocketHandleNewMessageImp } from '@Socket/HandleNewMessage'
+import { SocketNewMessageImp } from '@Socket/NewMessage'
 
 export interface IUserData {
   user: string
@@ -21,25 +19,21 @@ export interface SocketHandlerImp {
 export class SocketHandler {
   private readonly MAIN_CHANNEL: string
   private readonly _io: Server
-  private readonly _socketAddMessage: SocketAddMessageImp
   private readonly _socketGetMessage: SocketGetMessageImp
-  private readonly _socketPublishMessage: SocketPublishMessageImp
   private readonly _socketSubscribeToChannel: SocketSubscribeToChannelImp
   private readonly _socketHandleWelcome: SocketWelcomeImp
   private readonly _socketBroadcastNewConnection: SocketBroadcastNewConnectionImp
   private readonly _socketEmitMessages: SocketEmitMessagesImp
-  private readonly _socketHandleNewMessage: SocketHandleNewMessageImp
+  private readonly _socketNewMessage: SocketNewMessageImp
 
   constructor(
     httpServer: httpServer,
-    socketAddMessage: SocketAddMessageImp,
     socketGetMessage: SocketGetMessageImp,
-    socketPublishMessage: SocketPublishMessageImp,
     socketSubscribeToChannel: SocketSubscribeToChannelImp,
     socketHandleWelcome: SocketWelcomeImp,
     socketBroadcastNewConnection: SocketBroadcastNewConnectionImp,
     socketEmitMessages: SocketEmitMessagesImp,
-    socketHandleNewMessage: SocketHandleNewMessageImp,
+    socketNewMessage: SocketNewMessageImp,
   ) {
     this.MAIN_CHANNEL = 'geral'
 
@@ -55,14 +49,12 @@ export class SocketHandler {
     io.listen(Number(process.env.SERVER_PORT))
 
     this._io = io
-    this._socketAddMessage = socketAddMessage
     this._socketGetMessage = socketGetMessage
-    this._socketPublishMessage = socketPublishMessage
     this._socketSubscribeToChannel = socketSubscribeToChannel
     this._socketHandleWelcome = socketHandleWelcome
     this._socketBroadcastNewConnection = socketBroadcastNewConnection
     this._socketEmitMessages = socketEmitMessages
-    this._socketHandleNewMessage = socketHandleNewMessage
+    this._socketNewMessage = socketNewMessage
   }
 
   private async handleConnection(socket: Socket) {
@@ -75,7 +67,7 @@ export class SocketHandler {
     const messages = await this._socketGetMessage.handle(this.MAIN_CHANNEL)
     this._socketEmitMessages.handle(socket, messages)
 
-    socket.on('new_message', this._socketHandleNewMessage.handle)
+    socket.on('new_message', this._socketNewMessage.handle)
   }
 
   public init(): void {
